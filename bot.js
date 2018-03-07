d73c6779beea41dbea74d7b7b1d65ebb33dfce2a
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-03-07 11:51:09 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('message', message => {
 						}).catch(err => console.log(err.stack))
 						break
 						
-					case `${args[0].startsWith('@')}`: //mentioned user(s)
+					case message.mentions.USERS_PATTERN: //mentioned user(s)
 						let mentioned = message.mentions.users.array()
 						mentioned.forEach((user, index) => {
 							message.reply(`Cleaning up messages from ${user.username}`)
