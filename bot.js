ae5b7c82f8c12e823db2c798a6ba66ad4e8c26dc
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-03-07 11:13:47 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,53 +27,52 @@ client.on('message', message => {
 	if (message.type === 'dm') return
 
 	const args = message.content.split(/\s+/g)
+	const command = args.shift().slice(prefix.length)
 	const symbols = new RegExp(/^[-!$%^&()_+|~={}\[\]:";'?,.\/]/)
 	
-	switch(args[0]) {
-		case `${prefix}cleanup`:
+	switch(command) {
+		case 'cleanup':
 			if (message.member.hasPermission('MANAGE_MESSAGES', false, true, true)) {
-				if(args.length > 1) {
-					switch(args[1]) {
-						case 'commands': //all messages that begin with the most common symbols used in commands
+				switch(args[0]) {
+					case 'commands': //all messages that begin with the most common symbols used in commands
+						message.channel.fetchMessages({ limit: 100 })
+						.then(messages => {
+							let msgs = messages.filter(msg => msg.content.startsWith(symbols))
+							message.channel.bulkDelete(msgs)
+						}).catch(err => console.log(err.stack))
+						break
+			
+					case 'bots': //all messages that are posted by bots
+						message.channel.fetchMessages({ limit: 100 })
+						.then(messages => {
+							let msgs = messages.filter(msg => msg.author.bot)
+							message.channel.bulkDelete(msgs)
+						}).catch(err => console.log(err.stack))
+						break
+						
+					case 'all': //all past 100 messages
+						message.channel.fetchMessages({ limit: 100 })
+						.then(messages => {
+							message.channel.bulkDelete(messages)
+						}).catch(err => console.log(err.stack))
+						break
+						
+					case user: //mentioned user(s)
+						let mentioned = message.mentions.users.array()
+						mentioned.forEach((user, index) => {
 							message.channel.fetchMessages({ limit: 100 })
-							.then(messages => {
-								let msgs = messages.filter(msg => msg.content.startsWith(symbols))
+								.then(messages => {
+								let msgs = messages.filter(msg => msg.author.id === user.id)
 								message.channel.bulkDelete(msgs)
 							}).catch(err => console.log(err.stack))
-							break
-				
-						case 'bots': //all messages that are posted by bots
-							message.channel.fetchMessages({ limit: 100 })
-							.then(messages => {
-								let msgs = messages.filter(msg => msg.author.bot)
-								message.channel.bulkDelete(msgs)
-							}).catch(err => console.log(err.stack))
-							break
-							
-						case 'all': //all past 100 messages
-							message.channel.fetchMessages({ limit: 100 })
-							.then(messages => {
-								message.channel.bulkDelete(messages)
-							}).catch(err => console.log(err.stack))
-							break
-							
-						case user: //mentioned user(s)
-							let mentioned = message.mentions.users.array()
-							mentioned.forEach((user, index) => {
-								message.channel.fetchMessages({ limit: 100 })
-									.then(messages => {
-									let msgs = messages.filter(msg => msg.author.id === user.id)
-									message.channel.bulkDelete(msgs)
-								}).catch(err => console.log(err.stack))
-							})
-							break
-							
-						default: //reply with possible commands
-							message.reply(`Command Usage: *\`${prefix}cleanup (commands/@mention/bots/all)\`* | \`(required)\` \`<optional>\``).then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
-					}	
-				} else {
-					message.reply(`Command Usage: *\`${prefix}cleanup (commands/@mention/bots/all)\`* | \`(required)\` \`<optional>\``).then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
-				}
+						})
+						break
+						
+					default: //reply with possible commands
+						message.reply(`Command Usage: *\`${prefix}cleanup (commands/@mention/bots/all)\`* | \`(required)\` \`<optional>\``).then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
+				}	
+			} else {
+				message.reply(`You have insufficient permissions to use this command`).then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))	
 			}
 			message.delete(0)
 			break
