a482dec8677e95c42a0959d30731300d65f5b852
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-03-07 21:30:59 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ client.on('message', message => {
 					case 'commands': //all messages that begin with the most common symbols used in commands
 						message.channel.fetchMessages({ limit: 100 })
 						.then(messages => {
-							let msgs = messages.filter(msg => symbols.test(msg.content))
+							let msgs = messages.filter(msg => symbols.test(msg.content) && msg.createdAt > new Date(Date.now - 12096e5))
 							message.channel.bulkDelete(msgs).catch(err => message.reply(err))
 							deleted_messages += parseInt(msgs.size)
 						}).catch(err => console.log(err.stack))
@@ -46,7 +46,7 @@ client.on('message', message => {
 					case 'bots': //all messages that are posted by bots
 						message.channel.fetchMessages({ limit: 100 })
 						.then(messages => {
-							let msgs = messages.filter(msg => msg.author.bot)
+							let msgs = messages.filter(msg => msg.author.bot && msg.createdAt > new Date(Date.now - 12096e5))
 							message.channel.bulkDelete(msgs).catch(err => console.log(err.stack))
 							deleted_messages += parseInt(msgs.size)
 						}).catch(err => console.log(err.stack))
@@ -55,7 +55,8 @@ client.on('message', message => {
 					case 'all': //all past 100 messages
 						message.channel.fetchMessages({ limit: 100 })
 						.then(messages => {
-							message.channel.bulkDelete(messages).catch(err => console.log(err.stack))
+							let msgs = message.filer(msg => msg.createdAt > new Date(Date.now - 12096e5))
+							message.channel.bulkDelete(msgs).catch(err => console.log(err.stack))
 							deleted_messages += parseInt(messages.size)
 						}).catch(err => console.log(err.stack))
 						break
@@ -69,7 +70,7 @@ client.on('message', message => {
 								mentioned_users.forEach((user, index) => {
 									message.channel.fetchMessages({ limit: 100 })
 										.then(messages => {
-											let msgs = messages.filter(msg => msg.author.id === user.id)
+											let msgs = messages.filter(msg => msg.author.id === user.id && msg.createdAt > new Date(Date.now - 12096e5))
 											message.channel.bulkDelete(msgs).catch(err => console.log(err.stack))
 											deleted_messages += parseInt(msgs.size)
 									}).catch(err => console.log(err.stack))
@@ -80,7 +81,7 @@ client.on('message', message => {
 								mentioned_roles.forEach((role, index) => {
 									message.channel.fetchMessages({ limit: 100 })
 										.then(messages => {
-											let msgs = messages.filter(msg => msg.member.roles.exists('id', role.id))
+											let msgs = messages.filter(msg => msg.member.roles.exists('id', role.id) && msg.createdAt > new Date(Date.now - 12096e5))
 											message.channel.bulkDelete(msgs).catch(err => console.log(err.stack))
 											deleted_messages += parseInt(msgs.size)
 									}).catch(err => console.log(err.stack))
