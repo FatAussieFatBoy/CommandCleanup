069dfd8ec2fdee364c4e3e61f39a1fbbdd92c404
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-03-07 22:09:26 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ client.on('message', message => {
 					case 'commands': //all messages that begin with the most common symbols used in commands
 						message.channel.fetchMessages({ limit: 100 })
 						.then(messages => {
-							let msgs = messages.filter(msg => symbols.test(msg.content) && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg != message)
+							let msgs = messages.filter(msg => symbols.test(msg.content) && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg.id != message.id)
 							
 							if(msgs.size === 0) return message.reply(`We could not find any messages. ***NOTE:*** *The bot cannot delete any messages posted more than 14 days ago...*`)
 								.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -49,7 +49,7 @@ client.on('message', message => {
 					case 'bots': //all messages that are posted by bots
 						message.channel.fetchMessages({ limit: 100 })
 						.then(messages => {
-							let msgs = messages.filter(msg => msg.author.bot && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg != message)
+							let msgs = messages.filter(msg => msg.author.bot && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg.id != message.id)
 							
 							if(msgs.size === 0) return message.reply(`We could not find any messages. ***NOTE:*** *The bot cannot delete any messages posted more than 14 days ago...*`)
 								.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -61,7 +61,7 @@ client.on('message', message => {
 					case 'all': //all past 100 messages
 						message.channel.fetchMessages({ limit: 100 })
 						.then(messages => {
-							let msgs = messages.filter(msg => msg.createdAt > new Date(Date.now() - 1.21e+9) && msg != message)
+							let msgs = messages.filter(msg => msg.createdAt > new Date(Date.now() - 1.21e+9) && msg.id != message.id)
 							
 							if(msgs.size === 0) return message.reply(`We could not find any messages. ***NOTE:*** *The bot cannot delete any messages posted more than 14 days ago...*`)
 								.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -79,7 +79,7 @@ client.on('message', message => {
 								mentioned_users.forEach((user, index) => {
 									message.channel.fetchMessages({ limit: 100 })
 										.then(messages => {
-											let msgs = messages.filter(msg => msg.author.id === user.id && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg != message)
+											let msgs = messages.filter(msg => msg.author.id === user.id && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg.id != message.id)
 											
 											if(msgs.size === 0) return message.reply(`We could not find any messages. ***NOTE:*** *The bot cannot delete any messages posted more than 14 days ago...*`)
 												.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -93,7 +93,7 @@ client.on('message', message => {
 								mentioned_roles.forEach((role, index) => {
 									message.channel.fetchMessages({ limit: 100 })
 										.then(messages => {
-											let msgs = messages.filter(msg => msg.member.roles.exists('id', role.id) && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg != message)
+											let msgs = messages.filter(msg => msg.member.roles.exists('id', role.id) && msg.createdAt > new Date(Date.now() - 1.21e+9) && msg.id != message.id)
 											
 											if(msgs.size === 0) return message.reply(`We could not find any messages. ***NOTE:*** *The bot cannot delete any messages posted more than 14 days ago...*`)
 												.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
