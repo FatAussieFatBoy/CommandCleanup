deb26c1d08b40b02052dc4d28c495b0d192ab0a0
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-03-12 01:33:02 +1000

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -6,11 +6,14 @@ module.exports.run = async (client, prefix, message, args, con, dbl) => {
 			if(!rows) return console.log(`ERROR: The database has no rows`)
 
 			let str = ''
-			let words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'keycap_ten']
+			let words = ['medal', 'trophy', 'second_place', 'third_place', 'medal', 'medal']
 			rows.forEach((row, index) => {
 				str += `\n:${words[index + 1]}: - Guild \`${rows[index].name}\` with a total of \`${rows[index].messages_deleted}\` messages deleted.\n`
 			})
+			
 			message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}`)
+				.then(msg => msg.delete(30 * 1000))
+				.catch(err => console.log(err.stack))
 		})
 	} else {
 		message.author.send(`This command is only available for users that have upvoted the bot, if you'd like to upvote the bot use \`${prefix}upvote\``).catch(err => console.log(err.stack))
