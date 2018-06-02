2d79ad3eb7047370185ed66624373832833e09be
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-06-02 20:53:15 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ con.connect(err => {
 client.on('message', async message => {
 	if (message.channel.type != 'dm') {
 		if (message.channel.permissionsFor(message.guild.member(client.user)).has('MANAGE_MESSAGES')) {
-			if (!message.member.hasPermission('ADMINISTRATOR', false, true, true)) {
+			if (!message.guild.member(message.author).hasPermission('ADMINISTRATOR', false, true, true)) {
 			    	if (/(?:https:?\/)?discord(?:app.com\/invite|.gg)/gi.test(message.content)) {
 				    	message.delete(0)
 				    	return
