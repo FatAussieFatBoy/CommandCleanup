f4cf58cdd6bb927b9a5dc21a55d653c4e77278bf
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-09-21 09:58:30 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,8 +26,7 @@ var con = mysql.createConnection({
 	host: process.env.SQL_HOST,
 	user: process.env.SQL_USER,
 	password: process.env.SQL_PASS,
-	database: process.env.SQL_DATABASE,
-	port: process.env.SQL_PORT
+	database: process.env.SQL_DATABASE
 })
 
 con.connect(err => {
