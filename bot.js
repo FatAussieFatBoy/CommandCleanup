043d0ad13b7270917e14b7753f7799b4e2ec9dfd
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-09-24 22:26:05 +1000

    Updated code for self-hosted branch

--- bot.js
+++ bot.js
@@ -1,40 +1,15 @@
 const discord = require('discord.js')
-const DBL = require('dblapi.js')
 const request = require('request')
 const mysql = require('mysql')
 
 const client = new discord.Client({disableEveryone: true})
-const dbl = new DBL(process.env.DBL_TOKEN)
 
 const token = process.env.TOKEN
 const prefix = process.env.PREFIX
 
 //ready event
 client.on('ready', () => {
-	//set clients activity to show server count
-	client.user.setActivity(`.cleanup`, {type: 'LISTENING'})
-	
 	console.log(`Logged in as ${client.user.username}!`)
-	console.log(`Connected to ${client.guilds.size} servers`)
-	
-	client.setInterval(() => {
-		dbl.postStats(client.guilds.size)
-	}, 1800 * 1000)
-})
-
-var con = mysql.createConnection({
-	host: process.env.SQL_HOST,
-	user: process.env.SQL_USER,
-	password: process.env.SQL_PASS,
-	database: process.env.SQL_DATABASE
-})
-
-con.connect(err => {
-	if(err) {
-		console.log(err.stack)
-	} else {
-		console.log(`Connected to database`)
-	}
 })
 
 client.on('message', async message => {
@@ -65,30 +40,6 @@ client.on('message', async message => {
 		//console.log(err.stack) when debugging
 	}
 
-})
-
-//Client join Guild Event
-client.on('guildCreate', guild => {
-	guild.owner.send(`CommandCleanup has been added to your guild \`${guild.name}\`, if you'd like to see the features and commands for this bot please use the link provided below and also consider giving the bot an upvote...\nhttps://discordbots.org/bot/420013638468894731`)
-	console.log(`CommandCleanup was added to, Name:${guild.name} | ID:${guild.id} | Members:${guild.memberCount}`)
-})
-
-//Client leave Guild Event
-client.on('guildDelete', guild => {
-	console.log(`CommandCleanup removed from, Name:${guild.name} | ID:${guild.id}`)
-	con.query(`SELECT * FROM guilds WHERE id = '${guild.id}'`, (err, rows) => {
-		if(err) { console.log(err.stack) } else {
-
-			let sql
-			
-			if(rows.length > 0) {
-				sql = `DELETE FROM guilds WHERE id = '${guild.id}'`
-				console.log(`Database table for guild ${guild.name} deleted`)
-				
-				con.query(sql)
-			}
-		}
-	})
 })
 
 client.login(token)
