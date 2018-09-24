555089652bb9934d484982b471ab1de8a5b8f5bd
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-09-24 22:35:25 +1000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
-//Invite Command
-module.exports.run = (client, prefix, message, args, con, dbl) => {
+/* Invite Command (Disabled by default for self hosted version of bot)
+module.exports.run = (client, prefix, message, args) => {
 	client.generateInvite(['SEND_MESSAGES', 'MANAGE_MESSAGES'])
 		.then(link => {
 			message.author.send(`If you'd like to add CommandCleanup to your server please use the link below...\n${link}`).catch(err => console.log(err.stack))
@@ -10,3 +10,4 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 		message.delete(0).catch(err => console.log(err.stack))
 	}
 }
+*/
