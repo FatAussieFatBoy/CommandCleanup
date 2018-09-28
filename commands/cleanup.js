f146da0bd942b78597619e826517c36340a876c3
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-09-29 00:14:43 +1000

    Update cleanup.js

--- commands/cleanup.js
+++ commands/cleanup.js
@@ -35,7 +35,7 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 				case 'commands': //all messages that begin with the most common symbols used in commands
 					message.channel.fetchMessages({ limit: 100 })
 					.then(messages => {
-						let msgs = messages.filter(msg => symbols.test(msg.content.substring(0, 2)) && msg.id != message.id && message.createdTimestamp >= date_limit)
+						let msgs = messages.filter(msg => symbols.test(msg.content.substring(0, 2)) && msg.id != message.id && msg.createdTimestamp >= date_limit)
 						
 						if(msgs.size === 0) return message.author.send(`We could not find any command messages inside \`#${message.channel.name}\`. ***NOTE:*** *The bot cannot delete any messages posted more than 14 days ago...*`)
 							.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -52,7 +52,7 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 				case 'bots': //all messages that are posted by bots
 					message.channel.fetchMessages({ limit: 100 })
 					.then(messages => {
-						let msgs = messages.filter(msg => msg.author.bot && msg.pinned == false && msg.id != message.id && message.createdTimestamp >= date_limit)
+						let msgs = messages.filter(msg => msg.author.bot && msg.pinned == false && msg.id != message.id && msg.createdTimestamp >= date_limit)
 						
 						if(msgs.size === 0) return message.author.send(`We could not find any messages posted by bots inside \`#${message.channel.name}\`.\n***NOTE:*** *The bot cannot delete any messages posted more than 14 days old...*`)
 							.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -69,7 +69,7 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 				case 'all': //all past 100 messages
 					message.channel.fetchMessages({ limit: 100 })
 					.then(messages => {
-						let msgs = messages.filter(msg => msg.pinned == false && msg.id != message.id && message.createdTimestamp >= date_limit)
+						let msgs = messages.filter(msg => msg.pinned == false && msg.id != message.id && msg.createdTimestamp >= date_limit)
 						
 						if(msgs.size === 0) return message.author.send(`We could not find any messages inside \`#${message.channel.name}\`.\n***NOTE:*** *The bot cannot delete any messages posted more than 14 days old...*`)
 							.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -86,11 +86,11 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 				case 'links': //all messages that start with http or https
 					message.channel.fetchMessages({ limit: 100 })
 					.then(messages => {
-						let msgs = messages.filter(msg => msg.content.includes('http://') || msg.content.includes('https://') && msg.pinned == false && msg.id != message.id && message.createdTimestamp >= date_limit)
+						let msgs = messages.filter(msg => msg.content.includes('http://') || msg.content.includes('https://') && msg.pinned == false && msg.id != message.id && msg.createdTimestamp >= date_limit)
 						
 						if(msgs.size === 0) return message.author.send(`We could not find any messages with links inside \`#${message.channel.name}\`.\n***NOTE:*** *The bot cannot delete any messages posted more than 14 days old...*`)
 							.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
-						message.channel.bulkDelete(msgs.first(num))
+						message.channel.bulkDelete(msgs.first(num), true)
 							.then(deleted_msgs => UpdateDeletedMessages(message.guild, deleted_msgs.size))
 							.catch(err => { 
 								console.log(err.stack);
@@ -103,7 +103,7 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 				case 'attachments': //all messages with attachments
 					message.channel.fetchMessages({ limit: 100 })
 					.then(messages => {
-						let msgs = messages.filter(msg => msg.attachments.size > 0 && msg.pinned == false && msg.id != message.id && message.createdTimestamp >= date_limit)
+						let msgs = messages.filter(msg => msg.attachments.size > 0 && msg.pinned == false && msg.id != message.id && msg.createdTimestamp >= date_limit)
 	
 						if(msgs.size === 0) return message.author.send(`We could not find any messages with attachments inside \`#${message.channel.name}\`.\n***NOTE:*** *The bot cannot delete any messages posted more than 14 days old...*`)
 							.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -120,7 +120,7 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 				case 'text': //all messages without attachments or links
 					message.channel.fetchMessages({ limit: 100 })
 					.then(messages => {
-						let msgs = messages.filter(msg => msg.attachments.size === 0 && !msg.content.includes('https://') && msg.pinned == false && !msg.content.includes('http://') && msg.id != message.id && message.createdTimestamp >= date_limit)
+						let msgs = messages.filter(msg => msg.attachments.size === 0 && !msg.content.includes('https://') && msg.pinned == false && !msg.content.includes('http://') && msg.id != message.id && msg.createdTimestamp >= date_limit)
 						
 						if(msgs.size === 0) return message.author.send(`We could not find any messages containing only text inside \`#${message.channel.name}\`.\n***NOTE:*** *The bot cannot delete any messages posted more than 14 days old...*`)
 							.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -143,7 +143,7 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 							mentioned_users.forEach((user, index) => {
 								message.channel.fetchMessages({ limit: 100 })
 									.then(messages => {
-										let msgs = messages.filter(msg => msg.author.id === user.id && msg.pinned == false && msg.id != message.id && message.createdTimestamp >= date_limit)
+										let msgs = messages.filter(msg => msg.author.id === user.id && msg.pinned == false && msg.id != message.id && msg.createdTimestamp >= date_limit)
 										
 										if(msgs.size === 0) return message.author.send(`We could not find any messages from that user inside \`#${message.channel.name}\`.\n***NOTE:*** *The bot cannot delete any messages posted more than 14 days old...*`)
 											.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
@@ -162,7 +162,7 @@ module.exports.run = (client, prefix, message, args, con, dbl) => {
 							mentioned_roles.forEach((role, index) => {
 								message.channel.fetchMessages({ limit: 100 })
 									.then(messages => {
-										let msgs = messages.filter(msg => msg.member.roles.exists('id', role.id) && msg.pinned == false && msg.id != message.id && message.createdTimestamp >= date_limit)
+										let msgs = messages.filter(msg => msg.member.roles.exists('id', role.id) && msg.pinned == false && msg.id != message.id && msg.createdTimestamp >= date_limit)
 										
 										if(msgs.size === 0) return message.author.send(`We could not find any messages from that role inside \`#${message.channel.name}\`.\n***NOTE:*** *The bot cannot delete any messages posted more than 14 days old...*`)
 											.then(msg => msg.delete(10 * 1000)).catch(err => console.log(err.stack))
