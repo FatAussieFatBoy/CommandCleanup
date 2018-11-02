b06b6f19f1b4961bfee9e8d099b27c8e4a28f8d2
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-11-02 13:38:38 +1000

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -5,19 +5,21 @@ module.exports.run = async (client, prefix, message, args, pool, dbl) => {
 	let str = ''
 
 	pool.getConnection((err, conn) => {
-		conn.query(`SELECT * FROM guilds ORDER BY messages_deleted DESC LIMIT 5`, (err, rows) => {
-			if(err) console.log(err.stack)
-			if(!rows) return console.log(`ERROR: The database has no rows`)
-	
-			rows.forEach((row, index) => {
-				str += `\n${ranks[index + 1]} - Guild \`${rows[index].name}\` with a total of \`${rows[index].messages_deleted}\` messages deleted.\n`
+		if(conn) {
+			conn.query(`SELECT * FROM guilds ORDER BY messages_deleted DESC LIMIT 5`, (err, rows) => {
+				if(err) console.log(err.stack)
+				if(!rows) return console.log(`ERROR: The database has no rows`)
+		
+				rows.forEach((row, index) => {
+					str += `\n${ranks[index + 1]} - Guild \`${rows[index].name}\` with a total of \`${rows[index].messages_deleted}\` messages deleted.\n`
+				})
+				
+				message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}\nIf you're guild isn't listed here you can visit https://commandcleanup.com and use the global leaderboard to search for your guilds stats`)
+					.then(msg => msg.delete(30 * 1000))
+					.catch(err => console.log(err.stack))
+				
+				pool.releaseConnection(conn)
 			})
-			
-			message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}\nIf you're guild isn't listed here you can visit https://commandcleanup.com and use the global leaderboard to search for your guilds stats`)
-				.then(msg => msg.delete(30 * 1000))
-				.catch(err => console.log(err.stack))
-			
-			pool.releaseConnection(conn)
-		})		
+		}
 	})
 }
