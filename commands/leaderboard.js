f4edc9b637b3965bb092d6e26bad693de508fe81
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-11-02 12:16:36 +1000

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -15,7 +15,7 @@ module.exports.run = async (client, prefix, message, args, con, dbl) => {
 		con.release()
 	})
 	
-	await message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}\nIf you're guild isn't listed here you can visit https://commandcleanup.com and use the global leaderboard to search for your guilds stats`)
+	message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}\nIf you're guild isn't listed here you can visit https://commandcleanup.com and use the global leaderboard to search for your guilds stats`)
 		.then(msg => msg.delete(30 * 1000))
 		.catch(err => console.log(err.stack))
 			
