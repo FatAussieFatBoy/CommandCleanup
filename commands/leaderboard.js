67ace78feae6c61712d501a2392f73b1f5337fee
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-11-12 18:46:11 +1000

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -15,11 +15,31 @@ module.exports.run = async (client, prefix, message, args, pool, dbl) => {
 				})
 				
 				message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}\nIf you're guild isn't listed here you can visit https://commandcleanup.com and use the global leaderboard to search for your guilds stats`)
-					.then(msg => msg.delete(30 * 1000))
+					.then(msg => addDeleteReaction(msg))
 					.catch(err => console.log(err.stack))
 				
 				pool.releaseConnection(conn)
 			})
 		}
 	})
+	
+	//add a delete emoji to a message so it can be deleted
+	function addDeleteReaction(message) {
+		//add reaction to message so it can be deleted
+		message.react('❌').catch((err) => console.log(err.stack))
+	
+		//filter the collector and set the message to expire after 5 minutes.
+		const filter = (reaction, user) => user.id != message.author.id && reaction.emoji.name === '❌'
+		let collector = message.createReactionCollector(filter, {time: 300 * 1000})
+	
+		//when the reaction is collected by the collector stop the collector
+		collector.on('collect', (r) => {
+			collector.stop()
+		})
+	
+		//when the collector stops delete the message
+		collector.on('end', (collected) => {
+			if (message.deletable) message.delete(0)
+		})
+	}
 }
