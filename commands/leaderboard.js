c98b77f496d43f84605e0104bd5b749a3b7fec9a
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-11-13 18:53:53 +1000

    13/11/2018 Update
    This is the 'PURGE' update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,3 +1,6 @@
+//require functions so they can be called
+const tools = require('./tools.js')
+
 //Leaderboard Command
 module.exports.run = async (client, prefix, message, args, pool, dbl) => {
 	
@@ -5,41 +8,33 @@ module.exports.run = async (client, prefix, message, args, pool, dbl) => {
 	let str = ''
 
 	pool.getConnection((err, conn) => {
+		if (err) {
+			throw err
+			pool.releaseConnection(conn)
+		}
+
 		if(conn) {
 			conn.query(`SELECT * FROM guilds ORDER BY messages_deleted DESC LIMIT 5`, (err, rows) => {
-				if(err) console.log(err.stack)
-				if(!rows) return console.log(`ERROR: The database has no rows`)
-		
-				rows.forEach((row, index) => {
-					str += `\n${ranks[index + 1]} - Guild \`${rows[index].name}\` with a total of \`${rows[index].messages_deleted}\` messages deleted.\n`
-				})
-				
-				message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}\nIf you're guild isn't listed here you can visit https://commandcleanup.com and use the global leaderboard to search for your guilds stats`)
-					.then(msg => addDeleteReaction(msg))
-					.catch(err => console.log(err.stack))
-				
-				pool.releaseConnection(conn)
+				if(err) {
+					throw err
+					pool.releaseConnection(conn)
+				}
+
+				if(rows) {
+					rows.forEach((row, index) => {
+						str += `\n${ranks[index + 1]} - Guild \`${rows[index].name}\` with a total of \`${rows[index].messages_deleted}\` messages deleted.\n`
+					})
+					
+					message.author.send(`**--- CommandCleanup Leaderboard ---**\n${str}\nIf you're guild isn't listed here you can visit https://commandcleanup.com and use the global leaderboard to search for your guilds stats`)
+						.then(msg => tools.addDeleteReaction(msg))
+						.catch(err => console.log(err.stack))
+					
+					pool.releaseConnection(conn)
+				} else {
+					console.log(`ERROR: The database has no rows`)
+					pool.releaseConnection(conn)
+				}
 			})
 		}
 	})
-	
-	//add a delete emoji to a message so it can be deleted
-	function addDeleteReaction(message) {
-		//add reaction to message so it can be deleted
-		message.react('❌').catch((err) => console.log(err.stack))
-	
-		//filter the collector and set the message to expire after 5 minutes.
-		const filter = (reaction, user) => user.id != message.author.id && reaction.emoji.name === '❌'
-		let collector = message.createReactionCollector(filter, {time: 300 * 1000})
-	
-		//when the reaction is collected by the collector stop the collector
-		collector.on('collect', (r) => {
-			collector.stop()
-		})
-	
-		//when the collector stops delete the message
-		collector.on('end', (collected) => {
-			if (message.deletable) message.delete(0)
-		})
-	}
 }
