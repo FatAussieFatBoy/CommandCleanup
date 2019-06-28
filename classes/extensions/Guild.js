5b2d45dadbc928ed318f854f006544fbad0e1ad1
FatAussieFatBoy <n.barker@hotmail.com.au>
2019-06-28 19:59:49 +1000

    Create Guild.js

---
+++ classes/extensions/Guild.js
@@ -0,0 +1,11 @@
+const { Structures } = require('discord.js')
+
+module.exports = Structures.extend('Guild', Guild => {
+	class CleanupGuild extends Guild {
+		constructor(...args) {
+			super(...args)
+		}
+	}
+
+	return CleanupGuild
+})
