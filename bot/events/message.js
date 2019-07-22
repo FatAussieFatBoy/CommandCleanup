247a70854961b544be4e441089aa9c9c6e616dd6
FatAussieFatBoy <n.barker@hotmail.com.au>
2019-07-22 18:46:18 +1000

    Removed Level Checking

--- bot/events/message.js
+++ bot/events/message.js
@@ -16,17 +16,6 @@ module.exports = async (client, message) => {
 	if (!cmd.config.enabled) return message.channel.sendToUser(message.author, `Encountered an error,`, { embed: { color: 'ff0000', description: `The command **${cmd.help.name}** has been disabled.`}, delay: 5000  })
 	if (!message.guild && cmd.config.guildOnly) return message.channel.sendToUser(message.author, `Encountered an error,`, { embed: { color: 'ff0000', description: `The command **${cmd.help.name}** can only be used inside a guild.`, }, delay: 20000  })
 	
-	const level = getLevel(message)
-	if (level < cmd.config.permLevel) return message.channel.sendToUser(message.author, `Insufficient permissions,`, { embed: { color: 'ff0000', description: `You have insufficient permissions to use the command **\`${cmd.help.name}\`**.\nYour access level: \`${client.levels.find(l => l.level == level).name}\` | Required access level: \`${client.levels.find(l => l.level == cmd.config.permLevel).name}\`` }, delay: 20000 })
-
 	await cmd.run(client, message, args, level)
 	if (message.deletable) message.delete(0)
-}
-
-function getLevel(message) {
-	const order = message.client.levels.slice(0).sort((p, n) => p.level < n.level ? 1 : -1)
-	while (order.length) {
-		const currentLevel = order.shift()
-		if (currentLevel.check(message)) return currentLevel.level
-	}
 }
